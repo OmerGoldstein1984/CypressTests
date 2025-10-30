b229eac8141b3f16e28ceca110bb1a7c94f67d3c
LENOVO <omer.goldstein.1984@gmail.com>
2025-10-30 13:16:24 +0200

    pages added and modifications made to selectors in loginPage.cy.js to use data-qa attributes.

--- cypress/e2e/AutomationExcersice/addToCart.cy.js
+++ cypress/e2e/AutomationExcersice/addToCart.cy.js
@@ -1,21 +1,40 @@
 /// <reference types="cypress" />
+import loginPage from '../../support/PageObjects/loginPage.cy.js';
+import productPage from '../../support/PageObjects/productPage.cy.js';
+import cartPageCy from '@pages/cartPage.cy.js';
 describe('AutomationExcersice Add to Cart Tests', () => {
+    let user;
+
     before(function () {
         cy.fixture('user').then((data) => {
-            this.user = data;
+            user = data;
         });
-	});
-    it('adds a product to the cart', () => {
-        cy.login(this.user.user, this.user.password); // Custom command to log in
-        cy.contains('.productinfo h2', 'Rs. 500')           // Find the h2 with the specific text
-        .closest('.productinfo')                          // Go up to the product container
-        .find('.add-to-cart')                             // Find the specific Add to Cart button inside it
+    });
+
+    it('adds a product to the cart with cypress command', () => {
+        cy.login(user.user, user.password); 
+        cy.contains('.productinfo h2', 'Rs. 500')
+        .closest('.productinfo')                 
+        .find('.add-to-cart')                    
         .click();
-         cy.contains('Continue Shopping').click(); // Close the modal by clicking 'Continue Shopping'
-         cy.contains('.productinfo h2', 'Rs. 700')           // Find the h2 with the specific text
-        .closest('.productinfo')                          // Go up to the product container
-        .find('.add-to-cart')                             // Find the specific Add to Cart button inside it
+         cy.contains('Continue Shopping').click(); 
+         cy.contains('.productinfo h2', 'Rs. 700') 
+        .closest('.productinfo')                   
+        .find('.add-to-cart')                      
         .click();
-        cy.contains('Continue Shopping').click(); // Close the modal by clicking 'Continue Shopping'
-   });     
+        cy.contains('Continue Shopping').click(); 
+   });
+
+    it('adds a product to the cart with page object', () => {
+         loginPage.login(user.user, user.password);
+
+     productPage.addProductToCartByName('Rs. 500');
+         productPage.closeCartModal();
+
+     productPage.addProductToCartByName('Rs. 700');
+         productPage.closeCartModal();
+    
+    productPage.viewCart();     
+    });
+
 });
---
+++ cypress/support/PageObjects/cartPage.cy.js
@@ -0,0 +1,49 @@
+
+class CartPage {
+	// Locators
+	proceedToCheckoutButton = 'text=Proceed To Checkout';
+	cartTable = '#cart_info_table';
+	itemCell = '.cart_menu .image';
+	descriptionCell = '.cart_menu .description';
+	priceCell = '.cart_menu .price';
+	quantityCell = '.cart_menu .quantity';
+	totalCell = '.cart_menu .total';
+	productRow = (productId) => `#product-${productId}`;
+	productName = (productId) => `#product-${productId} .cart_description h4 a`;
+	productCategory = (productId) => `#product-${productId} .cart_description p`;
+	productPrice = (productId) => `#product-${productId} .cart_price p`;
+	productQuantity = (productId) => `#product-${productId} .cart_quantity button`;
+	productTotal = (productId) => `#product-${productId} .cart_total_price`;
+	productDeleteButton = (productId) => `#product-${productId} .cart_quantity_delete`;
+
+	// Methods
+	clickProceedToCheckout() {
+		cy.contains(this.proceedToCheckoutButton).click();
+	}
+
+	getProductName(productId) {
+		return cy.get(this.productName(productId));
+	}
+
+	getProductCategory(productId) {
+		return cy.get(this.productCategory(productId));
+	}
+
+	getProductPrice(productId) {
+		return cy.get(this.productPrice(productId));
+	}
+
+	getProductQuantity(productId) {
+		return cy.get(this.productQuantity(productId));
+	}
+
+	getProductTotal(productId) {
+		return cy.get(this.productTotal(productId));
+	}
+
+	deleteProduct(productId) {
+		cy.get(this.productDeleteButton(productId)).click();
+	}
+}
+
+export default new CartPage();
